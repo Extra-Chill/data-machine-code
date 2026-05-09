89d00cc543dfd39277af6ba7265a4680e07f011d
Chris Huber <chubes@chubes.net>
2026-05-09 13:04:52 -0400

    fix: clean diff summary formatting

--- inc/Workspace/Workspace.php
+++ inc/Workspace/Workspace.php
@@ -8341,7 +8341,8 @@ class Workspace {
 				continue;
 			}
 			$status = array_shift( $parts );
-			$path   = (string) end( $parts );
+			$path = (string) end( $parts );
+
 			$files[ $path ] = array(
 				'path'      => $path,
 				'old_path'  => count( $parts ) > 1 ? (string) $parts[0] : null,
