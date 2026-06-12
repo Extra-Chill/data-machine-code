1f595a8a05c21749b78b7271e1b265c18d9c68b0
Chris Huber <chubes@extrachill.com>
2026-06-12 19:09:22 -0400

    Fix disk budget lint failures

--- inc/Workspace/WorkspaceWorktreeLifecycle.php
+++ inc/Workspace/WorkspaceWorktreeLifecycle.php
@@ -776,7 +776,7 @@ trait WorkspaceWorktreeLifecycle {
 					$dirty_files = null;
 				}
 
-				$metadata_key     = null;
+				$metadata_key = null;
 				if ( ! $is_primary && $inside_ws ) {
 					$metadata_key = $relative;
 				} elseif ( ! $is_primary && ! $inside_ws ) {
